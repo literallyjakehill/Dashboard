ebfcacad0df35f5e1c4424e3707db37ba0d7e2c5
Jake Hill <literallyjakehill@gmail.com>
2025-10-30 12:23:25 +1300

    Update vite config and build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Dashboard/', // Remove the git URL from here
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
