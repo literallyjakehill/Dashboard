6a453b5a42e213f22cf2d2c87790131e4a5f311a
literallyjakehill <literallyjakehill@gmail.com>
2025-10-30 12:03:00 +1300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Dashboard/', https://github.com/literallyjakehill/
+  Dashboard.git
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
