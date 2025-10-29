e8f183d8440f63ed82d339b3fcc356def9abf5fa
Jake Hill <literallyjakehill@gmail.com>
2025-10-30 12:29:10 +1300

    Fix vite config and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Dashboard/', https://github.com/literallyjakehill/
-  Dashboard.git
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/Dashboard/', // Remove the git URL from here
+  base: '/Dashboard/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
-=======
 })
->>>>>>> origin/main
